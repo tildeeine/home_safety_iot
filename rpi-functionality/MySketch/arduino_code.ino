1f24c5d3e8760c04c879b4d4a6eb690ecd6e854e
tuvatholo <tuvavt@stud.ntnu.no>
2024-03-14 13:32:26 +0000

    testing to turn of buzzer from user input

--- rpi-functionality/MySketch/arduino_code.ino
+++ rpi-functionality/MySketch/arduino_code.ino
@@ -1,4 +1,4 @@
-void setup() {
+/*void setup() {
   Serial.begin(9600);
 }
 
@@ -10,6 +10,33 @@ void loop() {
       Serial.println("Command A received");
     } else if (incomingChar == 'B') {
       Serial.println("Command B received");
+    }
+  }
+}*/
+
+int pinTemp = A1;   //This is where our Output data goes
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int temp = analogRead(pinTemp);   
+  temp = temp * 0.48828125;   
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println("C"); 
+  delay(1000);
+
+    if (Serial.available() > 0) {
+    char incomingChar = Serial.read();
+ 
+    if (incomingChar == 'A') {
+      Serial.println("Command A received");
+    } else if (incomingChar == 'B') {
+      Serial.println("Command B received");
+      int pitch = map(temp,0,200, 50, 4000);
+      tone(8,pitch,20);
     }
   }
 }
