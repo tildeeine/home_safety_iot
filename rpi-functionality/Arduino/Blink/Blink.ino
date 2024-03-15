cf25d7bb0a0b996de8dee497d4b25a95e4cefd7d
tuvatholo <tuvavt@stud.ntnu.no>
2024-03-15 09:04:18 +0000

    debugging code

--- rpi-functionality/Arduino/Blink/Blink.ino
+++ rpi-functionality/Arduino/Blink/Blink.ino
@@ -7,7 +7,11 @@ void setup() {
 void loop() {
   int temp = analogRead(pinTemp);    //Read the analog pin
   temp = temp * 0.48828125;   // convert output (mv) to readable celcius
-   Serial.print("Tuva");
+  Serial.print("Temperature: ");
+  Serial.print("Tuva");
+  Serial.print(temp);
+  Serial.println("C");  //print the temperature status
+  delay(1000);
    
   if (Serial.available() > 0) {
     char incomingChar = Serial.read();
