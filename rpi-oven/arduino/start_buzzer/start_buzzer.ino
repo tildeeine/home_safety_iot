20cfe0fc1192669d43880f0523d64e381c6a6b73
Francisca Oliveira e Silva <franciscaoliveiraesilva@gmail.com>
2024-03-19 09:26:09 +0000

    Fixed merge conflicts

--- rpi-oven/arduino/start_buzzer/start_buzzer.ino
+++ rpi-oven/arduino/start_buzzer/start_buzzer.ino
@@ -14,15 +14,7 @@ void loop() {
   
   if (Serial.available() > 0) {
     char incomingChar = Serial.read();
-<<<<<<<< HEAD:rpi-oven/arduino/start_buzzer/start_buzzer.ino
     if (incomingChar == 'A') {
-========
-    Serial.println("Command received");
-
-    if (incomingChar == 'X') {
-      Serial.println("Command off received");
-    } else if (incomingChar == 'A') {
->>>>>>>> main:rpi-oven/Arduino/Blink/Blink.ino
       Serial.println("Command timer on received");
 	    int pitch = map(160,0,200, 50, 4000);
 	    tone(8,pitch,20);
