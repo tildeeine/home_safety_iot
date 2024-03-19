31527a14020b90badaa0ba791b59510a38d840c2
Francisca Oliveira e Silva <franciscaoliveiraesilva@gmail.com>
2024-03-19 18:25:17 +0000

    Test killswitch

--- rpi-oven/arduino/start_buzzer/start_buzzer.ino
+++ rpi-oven/arduino/start_buzzer/start_buzzer.ino
@@ -21,9 +21,18 @@ void loop() {
       Serial.println("Command timer on received");
 	    int pitch = map(160,0,200, 50, 4000);
 	    tone(8,pitch,20);
-      analogWrite(motorPin, 200);
-      delay(2000);
-      analogWrite(motorPin, 0);
+      
+      for(int x = 0; x <= 255; x++){
+      analogWrite(motorControl, x);
+      delay(50);
+    }
+
+    // ramp down the motor speed
+    for(int x = 255; x >= 0; x--){
+      analogWrite(motorControl, x);
+      delay(50);
+    }  
+    
     } else if (incomingChar == 'B') {
       Serial.println("Command kill switch received");
       analogWrite(motorPin, 50);
