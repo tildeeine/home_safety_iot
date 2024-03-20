1d8b11f2f23cab4145db137f46f6d68eb501ef54
tildeeine <t1ld3me@gmail.com>
2024-03-20 15:29:25 +0000

    test

--- rpi-oven/rpi/app.py
+++ rpi-oven/rpi/app.py
@@ -43,6 +43,7 @@ def alert_time():
         try:
             new_duration = int(data['duration'])
             if new_duration > 0:
+                print("old end time:", timer_end_time)
                 timer_end_time += (new_duration-current_timer_duration)
                 print("New end time:", timer_end_time)
 
